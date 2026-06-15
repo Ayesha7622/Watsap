35f043b074533afe6c97f6e524bad1882e7d80a4
Ayesha7622 <saddiqa7676@gmail.com>
2026-06-15 22:48:09 +0500

    ccvbmcv

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:watsap/watsap.dart';
 
 void main() {
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Wweb());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Wweb() //Imag()
+        );
   }
 }
 
